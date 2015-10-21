bf329682b535957bfe891d56c531861b70ec9d0b
CoLo7 <gagu5735@740s-pl-125.local>
2015-10-21 09:11:45 -0600

    added getChatInput method

--- src/chat/controller/ChatController.java
+++ src/chat/controller/ChatController.java
@@ -1,11 +1,17 @@
 package chat.controller;
+
+import chat.view.ChatView;
+
 /**
  * Application controller for the Chatbot project
  * @author gagu5735
- * @version 1.0 10/21/15
+ * @version 1.11 10/21/15 fixed error 
  */
+
+
 public class ChatController 
-{
+{	
+	private ChatView myDisplay;
 
 	public void start()
 	{
---
+++ src/chat/view/ChatView.java
@@ -0,0 +1,13 @@
+package chat.view;
+import javax.swing.JOptionPane;
+
+public class ChatView 
+{
+	
+	public String getChatInput (String currentText)
+	{
+		String returnedText = "";
+		return returnedText;
+	}
+
+}
