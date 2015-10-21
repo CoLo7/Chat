c943fff7734c484d300da7a5189cdef320ac8de7
CoLo7 <gagu5735@740s-pl-125.local>
2015-10-21 08:29:29 -0600

    Made MVC and test packages also made Runner class.

---
+++ src/chat/controller/ChatController.java
@@ -0,0 +1,14 @@
+package chat.controller;
+/**
+ * Application controller for the Chatbot project
+ * @author gagu5735
+ * @version 1.0 10/21/15
+ */
+public class ChatController 
+{
+
+	public void start()
+	{
+		
+	}
+}
