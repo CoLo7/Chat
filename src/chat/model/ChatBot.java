8976eaf3995bbc7093e7d2d0b69c0077f8fa5ba3
CoLo7 <gagu5735@740s-pl-125.local>
2015-10-23 08:20:46 -0600

    Added lengthChecker of currentInput.

--- src/chat/model/ChatBot.java
+++ src/chat/model/ChatBot.java
@@ -42,7 +42,12 @@ public class Chatbot
 	 */
 	public boolean lengthChecker(String currentInput)
 	{
-		return false;
+		boolean hasLength = false;
+		if(currentInput != null && currentInput.length() > 0)
+		{
+			hasLength = true;
+		}
+		return hasLength;
 	}
 	
 	/**
